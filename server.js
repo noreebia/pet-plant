b06d588b253c5d0895a495ec3ce3fb1b3af86ff7
sangwonKim <llllssss94@naver.com>
2018-09-19 16:41:06 +0900

    server setup

---
+++ server.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
