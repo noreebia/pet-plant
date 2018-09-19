cd0810d65045a697d791375727a0b6e911d84de6
noreebia <noreebia@gmail.com>
2018-09-19 17:19:33 +0900

    "Add body-parser"

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
+    res.send('Hello World');
 })
 
+// app.post('/logs', function(req, res) {
+//     console.log("Request body: " + req.body);
+//     let log = req.body;
+// })
+
 var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
 })
