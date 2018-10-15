437853d3bf50202d17e9206902c1e40a70fdd07d
noreebia <noreebia@gmail.com>
2018-10-16 00:49:58 +0900

    Create individual user query function

--- src/DatabaseService.js
+++ src/DatabaseService.js
@@ -7,6 +7,28 @@ exports.getAllUsers = function (callback) {
     });
 };
 
+exports.getUserByEmail = function (userEmail, callback) {
+    let query = "SELECT * FROM user WHERE user_email = \"" + userEmail + "\"";
+    db.connection.query(query, function (err, rows) {
+        if (err) {
+            throw err
+        }
+        callback(rows)
+    })
+}
+
+exports.getUserByEmailPromiseTest = function (userEmail) {
+    return new Promise(function (resolve, reject) {
+        let query = "SELECT * FROM user WHERE user_email = \"" + userEmail + "\"";
+        db.connection.query(query, function (err, rows) {
+            if (err) {
+                reject(new Error("Error querying database"));
+            }
+            resolve(rows);
+        })
+    })
+}
+
 exports.saveUser = function (email, password, callback) {
     let query = "INSERT INTO user (user_id, user_email, user_password) VALUES (default , \'" + email + "\',\'" + password + "\')";
     db.connection.query(query, function (err, rows) {
--- src/routers/User.js
+++ src/routers/User.js
@@ -7,12 +7,35 @@ router.use(function (req, res, next) {
     next()
 })
 
-router.post('/', function (req, res) {
-    let user = req.body;
-    databaseService.saveUser(user.email, user.password, function (result) {
+router.get('/:userEmail',function(req, res){
+    console.log(req.params.userEmail);
+    databaseService.getUserByEmail(req.params.userEmail, function(result){
         console.log(result);
     });
-    res.send('successful!');
+})
+
+router.get('/test/:userEmail',function(req, res){
+    console.log(req.params.userEmail);
+    databaseService.getUserByEmailPromiseTest(req.params.userEmail).then(function(result){
+        res.json(result);
+    }, function(error){  
+        res.json({success: false, message: "Error occurred while querying database"});     
+    })
+})
+
+router.post('/', function (req, res) {
+    let user = req.body;
+    let responseDTO = {success: true, message: ""};
+    try{
+        databaseService.saveUser(user.email, user.password, function (result) {
+            console.log(result);
+        });    
+    } catch(err){
+        responseDTO.success = false;
+        responseDTO.message = "User creation failed";
+    } finally{
+        res.json(responseDTO);
+    }
 })
 
 module.exports = router;
