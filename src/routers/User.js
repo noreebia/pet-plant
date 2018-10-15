dcfe6468c607cfe0125d39da29c7e2ea45d2e791
noreebia <noreebia@gmail.com>
2018-10-16 01:45:24 +0900

    Add DTO class and modify all router functions to user Promises

--- src/routers/User.js
+++ src/routers/User.js
@@ -1,41 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const databaseService = require('../DatabaseService');
+let DTO = require('../dto');
 
 router.use(function (req, res, next) {
     console.log('Request for user resource arrived...');
-    next()
+    next();
 })
 
-router.get('/:userEmail',function(req, res){
-    console.log(req.params.userEmail);
-    databaseService.getUserByEmail(req.params.userEmail, function(result){
-        console.log(result);
-    });
-})
-
-router.get('/test/:userEmail',function(req, res){
-    console.log(req.params.userEmail);
-    databaseService.getUserByEmailPromiseTest(req.params.userEmail).then(function(result){
-        res.json(result);
-    }, function(error){  
-        res.json({success: false, message: "Error occurred while querying database"});     
-    })
+router.get('/:userEmail', function (req, res) {
+    databaseService.getUserByEmail(req.params.userEmail)
+    .then((result) => res.json(new DTO(true, result)))
+    .catch((error) => res.json(new DTO(false, error.message)))
 })
 
 router.post('/', function (req, res) {
-    let user = req.body;
-    let responseDTO = {success: true, message: ""};
-    try{
-        databaseService.saveUser(user.email, user.password, function (result) {
-            console.log(result);
-        });    
-    } catch(err){
-        responseDTO.success = false;
-        responseDTO.message = "User creation failed";
-    } finally{
-        res.json(responseDTO);
-    }
+    databaseService.saveUser(req.body.email, req.body.password)
+    .then(() => res.json(new DTO(true)))
+    .catch( (error) => res.json(new DTO(false, error.message)))
 })
 
 module.exports = router;
