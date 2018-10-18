f82a2d97abf38a12d1e4aa1d77df006eb5c7b59f
sangwonKim <llllssss94@naver.com>
2018-10-18 18:08:55 +0900

    routerrouter

--- src/routers/img-router.js
+++ src/routers/img-router.js
@@ -1,4 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const databaseService = require('../database-service');
-let DTO = require('../dto');
+
+
+router.get('/', function (req, res) {
+    res.send("!!");
+})
