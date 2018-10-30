8ce98ce07d65faf0b79b2841c04e87b3340adbfa
sangwonKim <llllssss94@naver.com>
2018-10-30 15:46:22 +0900

    image module add half

--- image_classification_module/image_classification_module.py
+++ image_classification_module/image_classification_module.py
@@ -10,45 +10,35 @@ from keras.preprocessing.image import ImageDataGenerator
 col_size = 56
 row_size = 56
 
-
-"""
-cv_img = cv2.imread('test.jpg', cv2.IMREAD_GRAYSCALE)
-blur = cv2.GaussianBlur(cv_img, (5, 5), 0)
-ret, binary = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
-edge = cv2.Canny(binary, 127, 200)
-
-(_, contours, _) = cv2.findContours(edge, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-mask = np.ones(cv_img.shape[:2], dtype="uint8") * 255
-
-# Draw the contours on the mask
-cv2.drawContours(mask, contours, -1, (0,255,0), 1)
-
-import matplotlib.pyplot as plt
-plt.imshow(edge)
-plt.show()
-
-cv2.imwrite("test_processed.jpg", edge)
-"""
-
 from keras.models import load_model
 model = load_model('petplant_image.h5')
 
-test_datagen = ImageDataGenerator(rescale=1./255)
 
-test_generator = test_datagen.flow_from_directory(
-        './data/test',
-        target_size=(col_size, row_size),    
-        batch_size=16,
-        class_mode=None,
-        shuffle=False)
 
-# 6. 모델 사용하기
-print("-- Predict --")
-output = model.predict_generator(test_generator, steps=1)
-np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
-output = np.argmax(output,axis=-1)[0]
-if output == 0:
-    print("This picture is Sansevieria")
-else:
-    print("This picture is spartyfilm")
+def main(filename = ""):
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    test_generator = test_datagen.flow_from_directory(
+            './images/'+ filename +'/upload',
+            target_size=(col_size, row_size),    
+            batch_size=16,
+            class_mode=None,
+            shuffle=False)
+
+    # 6. 모델 사용하기
+    print("-- Predict --")
+    output = model.predict_generator(test_generator, steps=1)
+    np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+    output = np.argmax(output,axis=-1)[0]
+    if output == 0:
+        print("This picture is Sansevieria")
+        return "Sansevieria"
+    else:
+        print("This picture is spartyfilm")
+        return "spartyfilm"
+
+if __name__ == "__main__":
+    import sys
+    name = sys.argv[1]
+
+    print(main(filename=name))
--- src/routers/image-router.js
+++ src/routers/image-router.js
@@ -1,11 +1,12 @@
 var express = require('express'),
     router = express.Router(),
     multer = require('multer'),
+    PythonShell = require('python-shell'),
     fs = require('fs');
 
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, './images/upload');
+        callback(null, './images/'+ str(file.originalname) +'/upload');
     },
     filename: function (req, file, callback) {
         callback(null, file.originalname);
@@ -20,7 +21,20 @@ router.post('/',function(req,res){
             console.log(err);
             return res.end("Error uploading file.");
         }
+        console.log(req)
         res.end("File is uploaded");
+        var options = {
+            mode: 'text',
+            pythonPath: '',
+            pythonOptions: ['-u'],
+            scriptPath: '',
+            args: [str()]
+        };
+        PythonShell.run('image_classification_module.py', options, function (err, results){
+            if (err) throw err;
+
+            console.log('results: %j', results);
+        });
     });
 });
 
