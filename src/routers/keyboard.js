b0d79cec7f93f534304bde895aa8c5f677926f59
sangwonKim <llllssss94@naver.com>
2018-11-01 17:30:03 +0900

    Update chatbot

--- src/routers/keyboard.js
+++ src/routers/keyboard.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var databaseService = require('../database-service');
 
 router.get('/', async function (req, res) {
     let answer = {
         "type": "buttons",
-        "buttons": ["자유롭게 대화하기", "현재 상태 보기"]
+        "buttons": ["식물과 자유롭게 대화하기", "식물의 현재 상태 보기"]
     };
     res.send(answer);
 });
